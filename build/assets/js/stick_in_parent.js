0714ce4ffba67334e6c169ca261a9298e5f0c5ab
sundgirl <sundgirl19@gmail.com>
2020-07-16 12:38:47 +0300

    removed assets

--- build/assets/js/stick_in_parent.js
+++
@@ -1,7 +0,0 @@
-// stick_in_parent.js
-// $(document).ready(function () {
-//     $(".rotateProductWrap.cssHeight, .advantagesAppLeftColumn").stick_in_parent({
-//         offset_top: $("header").innerHeight()
-//     });
-// });
-// stick_in_parent.js
